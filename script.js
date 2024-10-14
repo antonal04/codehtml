5d40180f249884e22746894ac722b5bf0d35f48f
antonal04 <antonpvt1033@gmail.com>
2024-10-14 10:19:57 +0530

    e-commerse web page

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    // You can add any validation or processing here if needed
+
+    // No need to prevent the default since the form will submit to confirmation.html
+});
